6b615f90493ad64bc6ef41429f0cceb66a7e6639
Carly Darcher <casdarcher@gmail.com>
2018-03-05 20:29:48 -0800

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/Twenty_sided_dice.png">
+    <RaceSelector/>
   </div>
 </template>
 
 <script>
+import RaceSelector from './components/RaceSelector'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    RaceSelector
+  }
 }
 </script>
 
 <style>
-#app {
+#app { 
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
+}
+
+body {
+  background: #E6E6DB;
 }
 </style>
+
+
+
